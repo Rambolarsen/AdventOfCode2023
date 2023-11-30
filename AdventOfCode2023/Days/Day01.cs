c91b486229b9c8d6710691400cc14ab83bf9d722
Lars-Erik Arnesen <lars-erik.arnesen@bouvet.no>
2023-11-30 19:12:18 +0100

    Add project files.

---
+++ AdventOfCode2023/Days/Day01.cs
@@ -0,0 +1,18 @@
+﻿using AoCHelper;
+using FileParser;
+
+namespace AdventOfCode2023.Days;
+
+public class Day01 : BaseDay
+{
+    private readonly ParsedFile _input;
+
+    public Day01()
+    {
+        _input = new ParsedFile(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+
+    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+}
